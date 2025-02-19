2d841aaf184adf54353c78f8388c65c35abeabb1
Mehroj <mekhrojmajidov@gmail.com>
2025-02-19 12:27:34 +0500

    Added models to Admin Panel

--- pharmacy_app/admin.py
+++ pharmacy_app/admin.py
@@ -2,3 +2,13 @@ from django.contrib import admin
 from pharmacy_app.models import *
 
 admin.site.register(Staff)
+admin.site.register(Sale)
+admin.site.register(Product)
+admin.site.register(Uom)
+admin.site.register(Category)
+admin.site.register(UomGroup)
+admin.site.register(ProductPriceHistory)
+admin.site.register(SaleProduct)
+admin.site.register(SalePayment)
+admin.site.register(BackupWarehouseProduct)
+admin.site.register(WarehouseProduct)
